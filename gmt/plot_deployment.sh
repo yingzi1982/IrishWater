8b189304b199a96c7ecc7f0ebda80f0764ab0c32
yingzi1982 <yingzi.ying@me.com>
2021-03-20 10:52:22 +0000

    pushing to Github

--- gmt/plot_deployment.sh
+++ gmt/plot_deployment.sh
@@ -75,7 +75,7 @@ gmt psbasemap -R$region -J$projection -Bxa2.0f1.0+l"Easting (km) " -Bya1.0f0.5+l
 gmt grdimage -R -J  -B $grd -C$cpt -O -K >> $ps
 gmt grdcontour $grd -R -J -C20 -A40+f8p+u" m" -Gd1.5i -O -K >> $ps
 awk '{ print $1/1000, $2/1000 }' $sr   | gmt psxy -R -J -Sa0.05i -Gred  -N -Wthinner,black -O -K >> $ps
-awk '{ print $1/1000, $2/1000 }' $rc   | gmt psxy -R -J -St0.05i -Gyellow  -N -Wthinner,black -O >> $ps
+awk 'NR<=1{ print $3/1000, $4/1000 }' $rc   | gmt psxy -R -J -St0.05i -Gyellow  -N -Wthinner,black -O >> $ps
 rm -f $grd $grad
 #-------------------------------------
 
