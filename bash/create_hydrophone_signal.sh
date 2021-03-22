05ba91217053e49ef41e1fa5d91d38d1bffdb89e
yingzi1982 <yingzi.ying@me.com>
2021-03-22 23:29:21 +0000

    pushing to Github

--- bash/create_hydrophone_signal.sh
+++ bash/create_hydrophone_signal.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-#./matlab.sh generate_hydrophone_signal.m
-#./octave.sh generate_signal_spectrum.m hydrophone_signal
+./matlab.sh generate_hydrophone_signal.m
+./octave.sh generate_signal_spectrum.m hydrophone_signal
 
 cd ../gmt
 ./plot_signal.sh hydrophone_signal
-
-cd ../bash
-./git.sh push
