4f9db36b89f227b81d31f31545ee40bd3c355942
yingzi1982 <yingzi.ying@me.com>
2021-03-23 23:38:12 +0000

    pushing to Github

--- gmt/plot_signal.sh
+++ gmt/plot_signal.sh
@@ -39,12 +39,17 @@ pdf=$figfolder$name.pdf
 
 originalxy=$backupfolder$name
 
-tmin=`gmt gmtinfo $originalxy -C | awk '{print $1}'`
-tmax=`gmt gmtinfo $originalxy -C | awk '{print $2}'`
 ymin=`gmt gmtinfo $originalxy -C | awk '{print $3}'`
 ymax=`gmt gmtinfo $originalxy -C | awk '{print $4}'`
-tmin=2.8
+if [ $name == 'hydrophone_signal' ]
+then
+tmin=`gmt gmtinfo $originalxy -C | awk '{print $1}'`
+tmax=`gmt gmtinfo $originalxy -C | awk '{print $2}'`
+elif [ $name == 'specfem_hydrophone_signal' ]
+then
+tmin=3.1
 tmax=`echo "(10+($tmin))" | bc -l`
+fi
 
 normalization=`echo $ymin $ymax | awk ' { if(sqrt($1^2)>(sqrt($2^2))) {print sqrt($1^2)} else {print sqrt($2^2)}}'`
 timeDuration=`echo "(($tmax)-($tmin))" | bc -l`
