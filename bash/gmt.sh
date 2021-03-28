cfdec53fb8c4ac01bd4491bc318a411714f69149
yingzi1982 <yingzi.ying@me.com>
2021-03-28 22:50:47 +0100

    pushing to Github

--- bash/gmt.sh
+++ bash/gmt.sh
@@ -3,7 +3,11 @@
 module purge
 
 cd ../gmt
+./plot_signal.sh hydrophone_signal
+#./plot_signal.sh specfem_hydrophone_signal
+./plot_virtual_airgun_source_signal.sh
+./plot_source_signal.sh
+./plot_sound_speed_slice.sh
 ./plot_deployment.sh
-./plot_wiggle.sh
-
+./plot_transmission_loss.sh
 cd -
