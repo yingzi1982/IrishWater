13db5c95d2d1ffc5bade14783b7f541fb8d87d0b
yingzi1982 <yingzi.ying@me.com>
2021-03-28 23:43:37 +0100

    pushing to Github

--- gmt/plot_deployment.sh
+++ gmt/plot_deployment.sh
@@ -75,12 +75,12 @@ gmt psbasemap -R$region -J$projection -Bxa2.0f1.0+l"Easting (km) " -Bya1.0f0.5+l
 gmt grdimage -R -J  -B $grd -C$cpt -O -K >> $ps
 gmt grdcontour $grd -R -J -C20 -A40+f8p+u" m" -Gd1.5i -O -K > $grdcontour
 cat $grdcontour >> $ps
-awk '{ print $1/1000, $2/1000 }' $sr   | gmt psxy -R -J -Sa0.05i -Gred  -N -Wthinner,black -O -K >> $ps
-awk 'NR<=1{ print $3/1000, $4/1000 }' $rc   | gmt psxy -R -J -St0.05i -Gyellow  -N -Wthinner,black -O -K >> $ps
-rm -f $grd $grad
 sr=`awk '{ print $1/1000, $2/1000 }' $sr`
 rc=`awk 'NR<=1{ print $3/1000, $4/1000 }' $rc`
 echo -e "$sr \n $rc" | gmt psxy -R -J  -N -Wthinner,black,.- -O >> $ps
+echo $sr   | gmt psxy -R -J -Sa0.05i -Gred  -N -Wthinner,black -O -K >> $ps
+echo $rc   | gmt psxy -R -J -St0.05i -Gyellow  -N -Wthinner,black -O -K >> $ps
+rm -f $grd $grad
 #-------------------------------------
 
 #colorbar_width=$height
