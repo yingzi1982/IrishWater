95ec932a0107056ed92002d29d329ee3f76046c8
yingzi1982 <yingzi.ying@me.com>
2021-03-29 02:09:14 +0100

    pushing to Github

--- gmt/plot_transmission_loss.sh
+++ gmt/plot_transmission_loss.sh
@@ -131,6 +131,12 @@ dr=`echo "$dx * $range/($xmax - $xmin)" | bc -l`
 inc=$dr/$dz
 width=`echo "$range/($xmax - $xmin)*$width" | bc -l`
 
+sr=$backupfolder\output_list_sources.txt
+rc=$backupfolder\output_list_stations.txt
+
+sr=`awk -v range="$range" '{ print 0, $3/1000 }' $sr`
+rc=`awk -v range="$range" 'NR<=1{ print range/1000, $5/1000 }' $rc`
+
 height=0.8
 projection=X-$width\i/$height\i
 
@@ -145,8 +151,8 @@ gmt grdimage -R -J -B $grd -C$cpt -O -K >> $ps
 #gmt psclip  -R -J -B -C -O -K >> $ps
 #cat ../backup/sediment_polygon | awk '{ print $1/1000,$2/1000}' | gmt psxy -R -J -Ggray80 -W1p,black -O -K >> $ps #-G-red -G+red 
 #cat ../backup/rock_polygon | awk '{ print $1/1000,$2/1000}' | gmt psxy -R -J -Ggray60 -W1p,black -O -K >> $ps #-G-red -G+red 
-#echo $sr | gmt psxy -R -J -Sa0.05i -Gred  -N -Wthinner,black -O -K >> $ps
-#echo $rc | gmt psxy -R -J -St0.05i -Gyellow  -N -Wthinner,black -O -K >> $ps
+echo $sr | gmt psxy -R -J -Sa0.05i -Gred  -N -Wthinner,black -O -K >> $ps
+echo $rc | gmt psxy -R -J -St0.05i -Gyellow  -N -Wthinner,black -O -K >> $ps
 #echo "(b)" | gmt pstext -R -J -F+cTR -N -O -K >> $ps
 rm -f $grd
 #-------------------------------------
