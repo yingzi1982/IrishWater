6e5c3d154b74b6a2ca05ec31ea764b5b9a9bb89a
yingzi1982 <yingzi.ying@me.com>
2021-05-14 13:03:27 +0100

    pushing to Github

--- gmt/plot_signal.sh
+++ gmt/plot_signal.sh
@@ -116,6 +116,8 @@ xmin=1
 xmax=300
 ymin=`awk -v xmin="$xmin" -v xmax="$xmax" '$1>=xmin&&$1<=xmax {print}' $originalxy | gmt gmtinfo -C | awk '{print $3-5}'`
 ymax=`awk -v xmin="$xmin" -v xmax="$xmax" '$1>=xmin&&$1<=xmax {print}' $originalxy | gmt gmtinfo -C | awk '{print $4+5}'`
+echo $ymin $ymax
+exit
 
 region=$xmin/$xmax/$ymin/$ymax
 offset=1.5i
