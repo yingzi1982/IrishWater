062850e4513fdd6da1e3426237021de8a0063592
yingzi1982 <yingzi.ying@me.com>
2021-07-02 14:07:14 +0100

    pushing to Github

--- octave/generate_hydrophone_signal.m
+++ octave/generate_hydrophone_signal.m
@@ -19,10 +19,11 @@ hydrophone_signal = load('../backup/Rx18A.txt');
 t = hydrophone_signal(:,1);
 
 cut_time_start = 102.8-0.5;
+cut_time_start = cut_time_start + 0;
 [cut_time_start cut_time_start_index]=findNearest(t,cut_time_start);
 
 %cut_time_end = cut_time_start+6;;
-cut_time_end = cut_time_start+1;;
+cut_time_end = cut_time_start+2.5;;
 [cut_time_end cut_time_end_index]=findNearest(t,cut_time_end);
 
 hydrophone_signal = hydrophone_signal(cut_time_start_index:cut_time_end_index,:);
