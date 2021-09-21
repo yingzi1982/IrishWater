6ffc3d41b1eff3281f86e36523f3284b7350bb72
yingzi1982 <yingzi.ying@me.com>
2021-09-22 00:55:57 +0100

    pushing to Github

--- bash/create_hydrophone_signal.sh
+++ bash/create_hydrophone_signal.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-./octave.sh generate_hydrophone_signal.m
+#for shootingNumbering in $(seq 1 9)
+for shootingNumbering in $(seq 1 1)
+do
+
+./octave.sh generate_hydrophone_signal.m $shootingNumbering
 
 ./octave.sh generate_signal_spectrum.m hydrophone_signal
 
 cd ../gmt
 ./plot_signal.sh hydrophone_signal
+done
--- octave/generate_hydrophone_signal.m
+++ octave/generate_hydrophone_signal.m
@@ -9,6 +9,7 @@ if length(arg_list) == 1
   shootingNumbering=arg_list{1};
   shootingNumbering=str2num(shootingNumbering);
 end
+shootingNumbering
 
 %audioFile='../backup/wav/MGE04220120140718_110400Ch2_eugene.wav';
 %audioInfo = audioinfo(audioFile);
@@ -24,26 +25,29 @@ end
 hydrophone_signal = load('../backup/Rx18A.txt');
 dlmwrite('../backup/total_measured_hydrophone_signal',hydrophone_signal,' ');
 
-dt = hydrophone_signal(2,1)-hydrophone_signal(1,1);
-fs=1/dt;
-
-[hydrophone_signal_pks hydrophone_signal_idx] = findpeaks(hydrophone_signal(:,2),"MinPeakHeight",100,...
-                              "MinPeakDistance",round(10*fs),"DoubleSided");
-t_peak=hydrophone_signal(hydrophone_signal_idx,1);
-shooting_time_delay=mean(diff(t_peak));
-
+%dt = hydrophone_signal(2,1)-hydrophone_signal(1,1);
+%fs=1/dt;
+%
+%[hydrophone_signal_pks hydrophone_signal_idx] = findpeaks(hydrophone_signal(:,2),"MinPeakHeight",100,...
+%                              "MinPeakDistance",round(10*fs),"DoubleSided");
+%t_peak=hydrophone_signal(hydrophone_signal_idx,1);
+%shooting_time_delay=mean(diff(t_peak));
 
 t = hydrophone_signal(:,1);
+t = t-t(1);
 
 %cut_time_start = 102.8-0.5;
 %cut_time_start = cut_time_start + 1;
 shooting_interval=12.5;
-cut_time_start = shooting_interval*(shootingNumbering-1); 
+initial=0;
+cut_time_start = shooting_interval*(shootingNumbering-1)+initial; 
 [cut_time_start cut_time_start_index]=findNearest(t,cut_time_start);
 
 %cut_time_end = cut_time_start+6;;
 cut_time_end = cut_time_start+shooting_interval;;
 [cut_time_end cut_time_end_index]=findNearest(t,cut_time_end);
+cut_time_start
+cut_time_end
 
 hydrophone_signal = hydrophone_signal(cut_time_start_index:cut_time_end_index,:);
 t = hydrophone_signal(:,1);
