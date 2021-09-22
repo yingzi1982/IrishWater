a53f5806e075aa19b3f145dec81f295c299ac539
yingzi1982 <yingzi.ying@me.com>
2021-09-22 01:02:44 +0100

    pushing to Github

--- octave/generate_hydrophone_signal.m
+++ octave/generate_hydrophone_signal.m
@@ -9,7 +9,6 @@ if length(arg_list) == 1
   shootingNumbering=arg_list{1};
   shootingNumbering=str2num(shootingNumbering);
 end
-shootingNumbering
 
 %audioFile='../backup/wav/MGE04220120140718_110400Ch2_eugene.wav';
 %audioInfo = audioinfo(audioFile);
@@ -39,15 +38,13 @@ t = t-t(1);
 %cut_time_start = 102.8-0.5;
 %cut_time_start = cut_time_start + 1;
 shooting_interval=12.5;
-initial=0;
+initial=2;
 cut_time_start = shooting_interval*(shootingNumbering-1)+initial; 
 [cut_time_start cut_time_start_index]=findNearest(t,cut_time_start);
 
 %cut_time_end = cut_time_start+6;;
 cut_time_end = cut_time_start+shooting_interval;;
 [cut_time_end cut_time_end_index]=findNearest(t,cut_time_end);
-cut_time_start
-cut_time_end
 
 hydrophone_signal = hydrophone_signal(cut_time_start_index:cut_time_end_index,:);
 t = hydrophone_signal(:,1);
