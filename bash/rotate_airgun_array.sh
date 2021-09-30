95f64edca5a8c6c2ea307032680349d9c29d1515
yingzi1982 <yingzi.ying@me.com>
2021-09-30 22:52:27 +0100

    pushing to Github

--- bash/rotate_airgun_array.sh
+++ bash/rotate_airgun_array.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 ./octave.sh rotate_airgun_array.m
+
+cd ../gmt
+./plot_airgun_array_deployment.sh
--- octave/rotate_airgun_array.m
+++ octave/rotate_airgun_array.m
@@ -8,6 +8,6 @@ xy = load('../backup/airgun_array_deployment');
 
 [theta,rho] = cart2pol(xy(:,1),xy(:,2));
 
-xy_rotated=pol2cart(theta-deg2rad(166-90),rho);
+xy_rotated=pol2cart(theta+deg2rad(90+(180-166)),rho);
 
 dlmwrite('../backup/airgun_array_deployment_rotated',xy_rotated,' ');
