4479f56fa5238029e42b35fad052a6910d0492b5
yingzi1982 <yingzi.ying@me.com>
2021-10-18 08:50:57 +0100

    pushing to Github

--- gmt/plot_signal_compare.sh
+++ gmt/plot_signal_compare.sh
@@ -71,10 +71,11 @@ ymax=`gmt gmtinfo $originalxy -C | awk '{print $4}'`
 normalization=`echo $ymin $ymax | awk ' { if(sqrt($1^2)>(sqrt($2^2))) {print sqrt($1^2)} else {print sqrt($2^2)}}'`
 timeDuration=`echo "(($xmax)-($xmin))" | bc -l`
 #region=$xmin/$xmax/-1/1
-region=0/$timeDuration/-1/1
+#region=0/$timeDuration/-1/1
+region=0/5.5/-1/1
 
 
-resampling=10
+resampling=1
 
 awk -v xmin="$xmin" -v resampling="$resampling" -v normalization="$normalization" 'NR%resampling==0 {print $1-xmin, $2/normalization}' $originalxy | gmt psxy -J$projection -R$region -Bxa4f2+l"Time (s)" -Bya1f0.5 -Wthin,black -Y$offset -O>> $ps
 
